b11f2bf73d88d83258f55df270efe3748a2d2d01
Wesley Akio <wesleyakio@tuntscorp.com>
2016-02-03 20:13:21 -0200

    damn typo

--- rest/index.php
+++ rest/index.php
@@ -7,9 +7,9 @@ require_once ("bootstrap.php");
 use Tunts\Rest\RestServer;
 
 $server = new RestServer('/directcall-server/rest');
-$server -> addRoute('/hello', 'GET', 'Tunts\SampleSampleWeb', 'hello', 'json');
-$server -> addRoute('/obj', 'GET', 'Tunts\SampleSampleWeb', 'obj', 'json');
-$server -> addRoute('/arr', 'GET', 'Tunts\SampleSampleWeb', 'arr', 'json');
-$server -> addRoute('/parameters/{param}', 'GET', 'Tunts\SampleSampleWeb', 'parameters', 'json');
+$server -> addRoute('/hello', 'GET', 'Tunts\Sample\SampleWeb', 'hello', 'json');
+$server -> addRoute('/obj', 'GET', 'Tunts\Sample\SampleWeb', 'obj', 'json');
+$server -> addRoute('/arr', 'GET', 'Tunts\Sample\SampleWeb', 'arr', 'json');
+$server -> addRoute('/parameters/{param}', 'GET', 'Tunts\Sample\SampleWeb', 'parameters', 'json');
 $server -> route();
 ?>
